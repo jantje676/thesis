2fb9d34420aaa2787605f489c1ef95c24157a6b8
Kenneth <kengoei@live.nl>
2020-03-11 15:13:17 +0100

    beta run

---
+++ test_beta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=example
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0:30:00
+#SBATCH --mem=10000M
+#SBATCH --partition=gpu_short
+#SBATCH --gres=gpu:1
+
+module load 2019
+module load Miniconda2/4.5.12
+cp -r $HOME/ $TMPDIR
+cp -r $HOME/thesis $TMPDIR
+
+source ~/.bashrc
+
+conda activate thesis
+
+srun python3 /$TMPDIR/thesis/Beta-VAE/main.py --dataset dresses --seed 2 --lr 5e-4 --beta1 0.9 --beta2 0.999 \
+    --objective B --model B --batch_size 64 --z_dim 10 --max_iter 1000 \
+    --C_stop_iter 1e5 --C_max 20 --gamma 100 --viz_name dresses --viz_on False --dset_dir /$TMPDIR/thesis/data/Fashion200K/pictures_only
+
+conda deactivate
