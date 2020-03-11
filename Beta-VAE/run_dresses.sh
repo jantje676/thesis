3797ea5bf849977b94193d78ff53ad5682802a0e
Kenneth <kengoei@live.nl>
2020-03-11 15:02:39 +0100

    m

--- Beta-VAE/run_dresses.sh
+++ Beta-VAE/run_dresses.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 python main.py --dataset dresses --seed 2 --lr 5e-4 --beta1 0.9 --beta2 0.999 \
-    --objective B --model B --batch_size 64 --z_dim 10 --max_iter 1.5e6 \
-    --C_stop_iter 1e5 --C_max 20 --gamma 100 --viz_name dresses 
+    --objective B --model B --batch_size 64 --z_dim 10 --max_iter 1000 \
+    --C_stop_iter 1e5 --C_max 20 --gamma 100 --viz_name dresses --viz_on False
