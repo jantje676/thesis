ec2b657138cb124c6908772af8573dd334123a0a
Kenneth <kengoei@live.nl>
2020-03-18 15:08:04 +0100

    m

--- SCAN/evaluate_model.py
+++ SCAN/evaluate_model.py
@@ -34,8 +34,8 @@ def main(args):
     matches_t2i = get_matches_t2i(rti[1], test_id2data, nr_examples)
 
     # plot image and caption together
-    show_plots(matches_i2t, len(matches_i2t), "i2t")
-    show_plots(matches_t2i, len(matches_t2i), "t2i")
+    show_plots(matches_i2t, len(matches_i2t), "i2t", model_path)
+    show_plots(matches_t2i, len(matches_t2i), "t2i", model_path)
 
 
 
@@ -95,7 +95,7 @@ def show_plots(matches, n_example, title):
         ax[-1].set_title(title + ":" + matches[i][1])  # set title
         plt.imshow(img)
 
-    plt.savefig('{}save_plots.png'.format(model_path))
+    plt.savefig('{}save_plots_{}.png'.format(model_path, title))
 
 
 
