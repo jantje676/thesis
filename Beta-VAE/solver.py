75a6ef9a64fc26d6bf355ab833b557467ddc450d
Kenneth <kengoei@live.nl>
2020-03-19 15:18:03 +0100

    m

--- Beta-VAE/solver.py
+++ Beta-VAE/solver.py
@@ -192,13 +192,12 @@ class Solver(object):
             for x in self.data_loader:
                 self.global_iter += 1
                 pbar.update(1)
-                print("beginning shape: ", x.shape)
                 # wrap variable with tensor and cuda if possible
                 x = Variable(cuda(x, self.use_cuda))
 
                 # forward pass
                 x_recon, mu, logvar = self.net(x)
-                print(x_recon.shape)
+
                 # define reconstruction loss
                 recon_loss = reconstruction_loss(x, x_recon, self.decoder_dist)
 
@@ -250,9 +249,6 @@ class Solver(object):
                 if self.global_iter%self.save_step == 0:
                     self.save_checkpoint('last')
                     pbar.write('Saved checkpoint(iter:{})'.format(self.global_iter))
-
-                if self.global_iter%50000 == 0:
-                    self.save_checkpoint(str(self.global_iter))
 
                 if self.global_iter >= self.max_iter:
                     out = True
