dcbb9baad602fbb79c5c452408ea32d2d13f123f
Kenneth <kengoei@live.nl>
2020-03-31 21:24:12 +0200

    M m

--- SCAN/model.py
+++ SCAN/model.py
@@ -239,7 +239,14 @@ def cosine_similarity(x1, x2, dim=1, eps=1e-8):
     w12 = torch.sum(x1 * x2, dim)
     w1 = torch.norm(x1, 2, dim)
     w2 = torch.norm(x2, 2, dim)
-    return (w12 / (w1 * w2).clamp(min=eps)).squeeze()
+    temp = (w12 / (w1 * w2).clamp(min=eps))
+    print(x1.shape)
+    print(x2.shape)
+    print(w1.shape)
+    print(w2.shape)
+    print("temp")
+    print(temp.shape)
+    return temp.squeeze()
 
 
 def xattn_score_t2i(images, captions, cap_lens, opt):
